1e2ebd0787082cd810be2b43e64752859480ac81
mainuromayer <mainuromayer@gmail.com>
2023-03-06 22:41:47 +0600

    php07.php added

---
+++ php07.php
@@ -0,0 +1,50 @@
+<?php
+
+class Shape{
+    protected $name;
+    protected $area;
+    public function __construct($n){
+        $this->name = $n;
+        $this->calculateArea();
+    }
+
+    public function getArea(){
+        echo "This {$this->name}'s area is {$this->area}\n";
+    }
+
+    public function calculateArea(){}
+}
+
+class Triangle extends Shape{
+    private $a, $b, $c;
+    public function __construct($a, $b, $c){
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+        parent::__construct("Tringle");
+    }
+
+    public function calculateArea(){
+        $perimeter = ($this->a + $this->b + $this->c) / 2;
+        $this->area = sqrt($perimeter * ($perimeter - $this->a) * ($perimeter - $this->b) * ($perimeter - $this->c));
+    }
+}
+
+class Rectangle extends Shape{
+    private $a, $b;
+    public function __construct($a, $b){
+        $this->a = $a;
+        $this->b = $b;
+        parent::__construct("Rectangle");
+    }
+
+    public function calculateArea(){
+        $this->area = $this->a * $this->b;
+    }
+}
+
+$rec = new Rectangle(2, 4);
+$rec->getArea();
+
+$tri = new Triangle(10, 12, 8);
+$tri->getArea();
